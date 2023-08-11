753fe659127f07326de817afd4186c9fd358ffb5
Charles Fleming <charlesabrahamfleming@gmail.com>
2023-08-10 23:48:17 -0400

    Moving files around

--- src/py_congress/models/config.py
+++
@@ -1,8 +0,0 @@
-from typing import Literal, Union
-from pydantic import BaseModel, StrictStr, HttpUrl
-
-class PyCongressConfig(BaseModel):
-    api_key: StrictStr
-    api_version: StrictStr = 'v3'
-    base_url: HttpUrl = "https://api.congress.gov/"
-    response_format: Union[Literal('json'), Literal('xml')] = "json"
